9cd779fe157ff0a1990aa310dfd6464e895c8654
Felipe Del Valle <del.felipe@yahoo.com.br>
2018-06-29 14:41:17 -0300

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ENV } from '@app/env';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  environment;
+
+  constructor(public navCtrl: NavController) {
+    this.environment = ENV.mode;
+  }
+
+}
